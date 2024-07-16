f1b566507846817a716570d26877754ada815641
mennamohamed0207 <menamohamed0207@gmail.com>
2024-07-17 02:31:29 +0300

    Add store with sort filtering

--- Day2/Store/store.js
+++ Day2/Store/store.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 const mainProducts=document.getElementById('product-list');
 const category=document.getElementById('categories');
 const form=document.getElementById('search-form');
+const sorting=document.getElementById('sortBy');
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const title = category.value.trim();
-    console.log(title);
     if (title) {
         try {
             await fetchData(title);
@@ -20,7 +20,7 @@ form.addEventListener('submit', async (event) => {
 });
 
 async function fetchData(filter){
-    if(filter=="")
+    if(filter=="all")
     {
         const url=`https://fakestoreapi.com/products`;
         try{
@@ -30,7 +30,6 @@ async function fetchData(filter){
             }else{
                  data=await response.json();
                 displayProducts(data);
-                console.log(data);
                 return data;
             }
         }catch(e){
@@ -45,7 +44,6 @@ async function fetchData(filter){
             }else{
                  data=await response.json();
                 displayProducts(data);
-                console.log(data);
                 return data;
             }
         }catch(e){
@@ -54,12 +52,29 @@ async function fetchData(filter){
     }
   
 }
-fetchData("");
+fetchData("all");
 
 function displayProducts(products){
     mainProducts.innerHTML='';
+    const sortingWay=sorting.value.trim();
+    console.log(sortingWay);
+    if(sortingWay=="sortByPrice"){
+        products.sort((a,b) => a.price - b.price);
+    }else if (sortingWay=="sortByAlpha")
+    {
+        //sort alphabetically according to title
+        products.sort((a,b) => {
+            if (a.title < b.title) {
+                return -1;
+            }
+            if (a.title > b.title) {
+                return 1;
+            }
+            return 0;
+        });
+    }
     products.forEach(product => {
-        
+       
         // productItem.classList.add('product-item');
         mainProducts.innerHTML+=`<div class="product-card">
         <img src="${product.image}" alt="${product.title}">
