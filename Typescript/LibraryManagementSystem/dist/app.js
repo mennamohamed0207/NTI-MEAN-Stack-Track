eba71216a2a8fd5ce7c249812d51e4d9207dcc95
mennamohamed0207 <menamohamed0207@gmail.com>
2024-07-21 14:33:03 +0300

    Sort function added

--- Typescript/LibraryManagementSystem/dist/app.js
+++ Typescript/LibraryManagementSystem/dist/app.js
@@ -1,25 +1,30 @@
 "use strict";
-function showCard() {
+let books = [
+    { title: "The Great Gatsby", author: "X", genre: "Fantasy", publicationDate: "1925", rating: 4.5 },
+    { title: "To Kill a Mockingbird", author: "Y", genre: "Non-Fiction", publicationDate: "1960", rating: 4.2 },
+    { title: "Pride and Prejudice", author: "X", genre: "Fantasy", publicationDate: "1813", rating: 4.8 },
+    { title: "The Catcher in the Rye", author: "Z", genre: "Non-Fiction", publicationDate: "1951", rating: 4.6 },
+    { title: "The Lord of the Rings", author: "Z", genre: "Fiction", publicationDate: "1954", rating: 4.9 }
+];
+function getTotalBooks() {
+    let total = document.getElementById("totalBooks");
+    total.innerHTML = books.length.toString();
+}
+function showCard(booksShown) {
     let table = document.getElementById("mytable");
-    let books = [
-        { title: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Classic", publicationDate: "1925", rating: 4.5 },
-        { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Classic", publicationDate: "1960", rating: 4.2 },
-        { title: "Pride and Prejudice", author: "Jane Austen", genre: "Classic", publicationDate: "1813", rating: 4.8 },
-        { title: "The Catcher in the Rye", author: "J.D. Salinger", genre: "Classic", publicationDate: "1951", rating: 4.6 },
-        { title: "The Lord of the Rings", author: "J.R.R. Tolkien", genre: "Fantasy", publicationDate: "1954", rating: 4.9 }
-    ];
+    table.innerHTML = "";
     console.log("hello");
-    for (let i = 0; i < books.length; i += 2) {
-        if (i == books.length - 1) {
+    for (let i = 0; i < booksShown.length; i += 2) {
+        if (i == booksShown.length - 1) {
             table.innerHTML += `<tr>
             <td>
             <div class="card" style="width: 36rem;">
       <div class="card-body">
-        <h5 class="card-title">${books[i].title}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">${books[i].author}</h6>
-        <p class="card-text">Genere : ${books[i].genre}</p>
-        <p class="card-text">Publication Date : ${books[i].publicationDate}</p>
-        <p class="card-text">Rating : ${books[i].rating}</p>
+        <h5 class="card-title">${booksShown[i].title}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i].author}</h6>
+        <p class="card-text">Genere : ${booksShown[i].genre}</p>
+        <p class="card-text">Publication Date : ${booksShown[i].publicationDate}</p>
+        <p class="card-text">Rating : ${booksShown[i].rating}</p>
         <button class="btn btn-primary">Edit</button>
         <button class="btn btn-primary">Delete</button>
       </div>
@@ -32,11 +37,11 @@ function showCard() {
         <td>
         <div class="card" style="width: 36rem;">
   <div class="card-body">
-    <h5 class="card-title">${books[i].title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">${books[i].author}</h6>
-    <p class="card-text">Genere : ${books[i].genre}</p>
-    <p class="card-text">Publication Date : ${books[i].publicationDate}</p>
-    <p class="card-text">Rating : ${books[i].rating}</p>
+    <h5 class="card-title">${booksShown[i].title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i].author}</h6>
+    <p class="card-text">Genere : ${booksShown[i].genre}</p>
+    <p class="card-text">Publication Date : ${booksShown[i].publicationDate}</p>
+    <p class="card-text">Rating : ${booksShown[i].rating}</p>
     <button class="btn btn-primary">Edit</button>
     <button class="btn btn-primary">Delete</button>
   </div>
@@ -45,11 +50,11 @@ function showCard() {
 <td>
 <div class="card" style="width: 36rem;">
  <div class="card-body">
-    <h5 class="card-title">${books[i + 1].title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">${books[i + 1].author}</h6>
-    <p class="card-text">Genere : ${books[i + 1].genre}</p>
-    <p class="card-text">Publication Date : ${books[i + 1].publicationDate}</p>
-    <p class="card-text">Rating : ${books[i + 1].rating}</p>
+    <h5 class="card-title">${booksShown[i + 1].title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i + 1].author}</h6>
+    <p class="card-text">Genere : ${booksShown[i + 1].genre}</p>
+    <p class="card-text">Publication Date : ${booksShown[i + 1].publicationDate}</p>
+    <p class="card-text">Rating : ${booksShown[i + 1].rating}</p>
     <button class="btn btn-primary">Edit</button>
     <button class="btn btn-primary">Delete</button>
     </td>
@@ -58,4 +63,107 @@ function showCard() {
         </tr>`;
     }
 }
-showCard();
+function getFictionBooks() {
+    let fiction = document.getElementById("fiction");
+    let fictionBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Fiction") {
+            fictionBooks++;
+        }
+    }
+    fiction.innerHTML = fictionBooks.toString();
+}
+function getNonFictionBooks() {
+    let Nonfiction = document.getElementById("nonfiction");
+    let NonfictionBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Non-Fiction") {
+            NonfictionBooks++;
+        }
+    }
+    Nonfiction.innerHTML = NonfictionBooks.toString();
+}
+function getFantasyBooks() {
+    let fantasy = document.getElementById("fantasy");
+    let fantasyBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Fantasy") {
+            fantasyBooks++;
+        }
+    }
+    fantasy.innerHTML = fantasyBooks.toString();
+}
+function getBooksByAuthorX() {
+    let authorX = document.getElementById("authorX");
+    let authorXBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "X") {
+            authorXBooks++;
+        }
+    }
+    authorX.innerHTML = authorXBooks.toString();
+}
+function getBooksByAuthorY() {
+    let authorY = document.getElementById("authorY");
+    let authorYBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "Y") {
+            authorYBooks++;
+        }
+    }
+    authorY.innerHTML = authorYBooks.toString();
+}
+function getBooksByAuthorZ() {
+    let authorZ = document.getElementById("authorZ");
+    let authorZBooks = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "Z") {
+            authorZBooks++;
+        }
+    }
+    authorZ.innerHTML = authorZBooks.toString();
+}
+function sortByTitle() {
+    books.sort(function (a, b) {
+        if (a.title < b.title) {
+            return -1;
+        }
+        if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+function sortByPublicationDate() {
+    books.sort(function (a, b) {
+        if (a.publicationDate < b.publicationDate) {
+            return -1;
+        }
+        if (a.publicationDate > b.publicationDate) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+function sortByRating() {
+    books.sort(function (a, b) {
+        if (a.rating < b.rating) {
+            return -1;
+        }
+        if (a.rating > b.rating) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+showCard(books);
+getTotalBooks();
+getFictionBooks();
+getNonFictionBooks();
+getFantasyBooks();
+getBooksByAuthorX();
+getBooksByAuthorY();
+getBooksByAuthorZ();
--- Typescript/LibraryManagementSystem/src/app.ts
+++ Typescript/LibraryManagementSystem/src/app.ts
@@ -1,26 +1,33 @@
-function showCard() {
+type Book = { title: string, author: string, genre: string, publicationDate: string, rating: number };
+let books: Book[] = [
+    { title: "The Great Gatsby", author: "X", genre: "Fantasy", publicationDate: "1925", rating: 4.5 },
+    { title: "To Kill a Mockingbird", author: "Y", genre: "Non-Fiction", publicationDate: "1960", rating: 4.2 },
+    { title: "Pride and Prejudice", author: "X", genre: "Fantasy", publicationDate: "1813", rating: 4.8 },
+    { title: "The Catcher in the Rye", author: "Z", genre: "Non-Fiction", publicationDate: "1951", rating: 4.6 },
+    { title: "The Lord of the Rings", author: "Z", genre: "Fiction", publicationDate: "1954", rating: 4.9 }
+]
+function getTotalBooks() {
+
+    let total = document.getElementById("totalBooks") as HTMLElement;
+    total.innerHTML = books.length.toString();
+}
+function showCard(booksShown:Book[]) {
+    
     let table = document.getElementById("mytable") as HTMLElement;
-    type Book = { title: string, author: string, genre: string, publicationDate: string, rating: number };
-    let books: Book[] = [
-        { title: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Classic", publicationDate: "1925", rating: 4.5 },
-        { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Classic", publicationDate: "1960", rating: 4.2 },
-        { title: "Pride and Prejudice", author: "Jane Austen", genre: "Classic", publicationDate: "1813", rating: 4.8 },
-        { title: "The Catcher in the Rye", author: "J.D. Salinger", genre: "Classic", publicationDate: "1951", rating: 4.6 },
-        { title: "The Lord of the Rings", author: "J.R.R. Tolkien", genre: "Fantasy", publicationDate: "1954", rating: 4.9 }
-    ]
+    table.innerHTML = "";
     console.log("hello");
 
-    for (let i = 0; i < books.length; i += 2) {
-        if(i==books.length-1){
+    for (let i = 0; i < booksShown.length; i += 2) {
+        if (i == booksShown.length - 1) {
             table.innerHTML += `<tr>
             <td>
             <div class="card" style="width: 36rem;">
       <div class="card-body">
-        <h5 class="card-title">${books[i].title}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">${books[i].author}</h6>
-        <p class="card-text">Genere : ${books[i].genre}</p>
-        <p class="card-text">Publication Date : ${books[i].publicationDate}</p>
-        <p class="card-text">Rating : ${books[i].rating}</p>
+        <h5 class="card-title">${booksShown[i].title}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i].author}</h6>
+        <p class="card-text">Genere : ${booksShown[i].genre}</p>
+        <p class="card-text">Publication Date : ${booksShown[i].publicationDate}</p>
+        <p class="card-text">Rating : ${booksShown[i].rating}</p>
         <button class="btn btn-primary">Edit</button>
         <button class="btn btn-primary">Delete</button>
       </div>
@@ -33,11 +40,11 @@ function showCard() {
         <td>
         <div class="card" style="width: 36rem;">
   <div class="card-body">
-    <h5 class="card-title">${books[i].title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">${books[i].author}</h6>
-    <p class="card-text">Genere : ${books[i].genre}</p>
-    <p class="card-text">Publication Date : ${books[i].publicationDate}</p>
-    <p class="card-text">Rating : ${books[i].rating}</p>
+    <h5 class="card-title">${booksShown[i].title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i].author}</h6>
+    <p class="card-text">Genere : ${booksShown[i].genre}</p>
+    <p class="card-text">Publication Date : ${booksShown[i].publicationDate}</p>
+    <p class="card-text">Rating : ${booksShown[i].rating}</p>
     <button class="btn btn-primary">Edit</button>
     <button class="btn btn-primary">Delete</button>
   </div>
@@ -46,11 +53,11 @@ function showCard() {
 <td>
 <div class="card" style="width: 36rem;">
  <div class="card-body">
-    <h5 class="card-title">${books[i + 1].title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">${books[i + 1].author}</h6>
-    <p class="card-text">Genere : ${books[i + 1].genre}</p>
-    <p class="card-text">Publication Date : ${books[i + 1].publicationDate}</p>
-    <p class="card-text">Rating : ${books[i + 1].rating}</p>
+    <h5 class="card-title">${booksShown[i + 1].title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">${booksShown[i + 1].author}</h6>
+    <p class="card-text">Genere : ${booksShown[i + 1].genre}</p>
+    <p class="card-text">Publication Date : ${booksShown[i + 1].publicationDate}</p>
+    <p class="card-text">Rating : ${booksShown[i + 1].rating}</p>
     <button class="btn btn-primary">Edit</button>
     <button class="btn btn-primary">Delete</button>
     </td>
@@ -60,4 +67,120 @@ function showCard() {
 
     }
 }
-showCard();
+function getFictionBooks() {
+
+    let fiction = document.getElementById("fiction") as HTMLElement;
+    let fictionBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Fiction") {
+            fictionBooks++;
+        }
+    }
+    fiction.innerHTML = fictionBooks.toString();
+}
+function getNonFictionBooks() {
+
+    let Nonfiction = document.getElementById("nonfiction") as HTMLElement;
+    let NonfictionBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Non-Fiction") {
+            NonfictionBooks++;
+        }
+    }
+    Nonfiction.innerHTML = NonfictionBooks.toString();
+}
+function getFantasyBooks() {
+
+    let fantasy = document.getElementById("fantasy") as HTMLElement;
+    let fantasyBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].genre == "Fantasy") {
+            fantasyBooks++;
+        }
+    }
+    fantasy.innerHTML = fantasyBooks.toString();
+}
+function getBooksByAuthorX() {
+
+    let authorX = document.getElementById("authorX") as HTMLElement;
+    let authorXBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "X") {
+            authorXBooks++;
+        }
+    }
+    authorX.innerHTML = authorXBooks.toString();
+}
+function getBooksByAuthorY() {
+
+    let authorY = document.getElementById("authorY") as HTMLElement;
+    let authorYBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "Y") {
+            authorYBooks++;
+        }
+    }
+    authorY.innerHTML = authorYBooks.toString();
+}
+function getBooksByAuthorZ() {
+
+    let authorZ = document.getElementById("authorZ") as HTMLElement;
+    let authorZBooks: number = 0;
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author == "Z") {
+            authorZBooks++;
+        }
+    }
+    authorZ.innerHTML = authorZBooks.toString();
+}
+
+function sortByTitle() {
+
+    books.sort(function (a, b) {
+        if (a.title < b.title) {
+            return -1;
+        }
+        if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+function sortByPublicationDate() {
+
+    books.sort(function (a, b) {
+        if (a.publicationDate < b.publicationDate) {
+            return -1;
+        }
+        if (a.publicationDate > b.publicationDate) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+function sortByRating() {
+
+    books.sort(function (a, b) {
+        if (a.rating < b.rating) {
+            return -1;
+        }
+        if (a.rating > b.rating) {
+            return 1;
+        }
+        return 0;
+    });
+    showCard(books);
+}
+showCard(books);
+getTotalBooks();
+getFictionBooks();
+getNonFictionBooks();
+getFantasyBooks();
+getBooksByAuthorX();
+getBooksByAuthorY();
+getBooksByAuthorZ();
+
+
+
