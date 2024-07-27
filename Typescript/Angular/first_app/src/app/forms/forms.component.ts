8091ba462445d8a1276ab1eb3f728ea72b60ac3b
mennamohamed0207 <menamohamed0207@gmail.com>
2024-07-27 12:04:48 +0300

    Form with validations

--- Typescript/Angular/first_app/src/app/forms/forms.component.ts
+++ Typescript/Angular/first_app/src/app/forms/forms.component.ts
@@ -7,8 +7,51 @@ import { NgForm } from '@angular/forms';
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
- onSubmit(myForm: NgForm) {
-  console.log("form submitted");
-  
-}
+  onSubmit(myForm: NgForm) {
+    
+    myForm.form.patchValue({
+      username: "mynewusername"
+    });
+    // myForm.setValue({
+    //   about: "",
+    //   age
+    //     :
+    //     "",
+    //   birthDate
+    //     :
+    //     "",
+    //   cv
+    //     :
+    //     "",
+    //   email
+    //     :
+    //     "",
+    //   gender
+    //     :
+    //     "",
+    //   linkedInURL
+    //     :
+    //     "",
+    //   password
+    //     :
+    //     "",
+    //   phone
+    //     :
+    //     "",
+    //   retypePassword
+    //     :
+    //     "",
+    //   status
+    //     :
+    //     "",
+    //   terms
+    //     :
+    //     true,
+    //   username
+    //     :
+    //     "mynewusername",
+    // });
+    console.log(myForm.value);
+
+  }
 }
