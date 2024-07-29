6b7abbc042d6383f8e4b490caff159cb078c291e
mennamohamed0207 <menamohamed0207@gmail.com>
2024-07-29 20:15:03 +0300

    Adding reactive forms but matched passwords and languages don't work

--- Typescript/Angular/first_app/src/app/reactive-forms/customValidation.ts
+++ Typescript/Angular/first_app/src/app/reactive-forms/customValidation.ts
@@ -2,19 +2,29 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export class customValidator {
 
-    static passwordValidator() :ValidatorFn{
-        return (control:AbstractControl):ValidationErrors | null=>{
+    static passwordValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
 
-            const value=control.value;
-            if(!value){
+            const value = control.value;
+            if (!value) {
                 return null;
             }
             const hasNumber = /[0-9]/.test(value);
             const hasUpper = /[A-Z]/.test(value);
             const hasLower = /[a-z]/.test(value);
-            const isValidLength=value.length>=8;
-            const isValid = hasNumber && hasUpper && hasLower&& isValidLength;
-            return !isValid ? {passwordStrength: true} : null;
+            const isValidLength = value.length >= 8;
+            const isValid = hasNumber && hasUpper && hasLower && isValidLength;
+            return !isValid ? { passwordStrength: true } : null;
         }
+    }
+    static matchPassword(control: AbstractControl): ValidationErrors | null {
+       
+            const password = control.get('password')?.value;
+            const retypePassword = control.get('retypePassword')?.value;
+            if (password !== retypePassword) {
+                return { matchPasswords: true };
+            }
+            return null;
+        
     }
 }
--- Typescript/Angular/first_app/src/app/reactive-forms/reactive-forms.component.ts
+++ Typescript/Angular/first_app/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,40 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { customValidator } from './customValidation';
+
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
-  styleUrl: './reactive-forms.component.css'
+  styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
-  //can be null 
   myForm!: FormGroup;
-  obj={
-    name:"menna",
-    age:22
-  };
+  // languages: FormArray = new FormArray([new FormControl(null)]);
 
-  //be excuted when component is created
   ngOnInit(): void {
     this.myForm = new FormGroup({
-      username: new FormControl(null,[Validators.required,Validators.minLength(3)]),
-      password: new FormControl(null,[Validators.required,customValidator.passwordValidator()]),
-      retypePassword: new FormControl(null),
+      username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      password: new FormControl(null, [Validators.required, customValidator.passwordValidator()]),
+      retypePassword: new FormControl(null, [Validators.required]),
       about: new FormControl(null),
-      email: new FormControl(null,[Validators.required,Validators.email]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       age: new FormControl(null),
       gender: new FormControl(null),
       linkedInURL: new FormControl(null),
       phone: new FormControl(null),
       birthDate: new FormControl(null),
-      status:new FormControl(null),
-      cv : new FormControl(null),
-      terms:new FormControl(null)
-    });
+      status: new FormControl(null),
+      cv: new FormControl(null),
+      terms: new FormControl(null),
+      //array of languages
+      languages: new FormArray([new FormControl(null)]),
+    }, { validators: customValidator.matchPassword });
   }
-  onSubmit(){
-    console.log(this.myForm.controls['email']);
+  get languages() {
+    let lang = this.myForm.get('languages') as FormArray;
+    return lang;
   }
 
+  addLang() {
+    this.languages.push(new FormControl(null));
+
+  }
+  onSubmit() {
+    console.log(this.myForm.controls['email']);
+  }
 }
