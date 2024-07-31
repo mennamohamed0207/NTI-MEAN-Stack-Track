8a9159cb4a377ad7cb45829325b325c70f7ce69c
mennamohamed0207 <menamohamed0207@gmail.com>
2024-07-31 12:13:42 +0300

    Adding switch

--- Typescript/Angular/first_app/src/app/super/dervied/dervied.component.ts
+++ Typescript/Angular/first_app/src/app/super/dervied/dervied.component.ts
@@ -8,9 +8,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class DerviedComponent {
   @Input()product?: {name:string,age:number,id:number};
   @Output()like :EventEmitter<void>=new EventEmitter<void>();
+  htmlcontent: string | any;
 
   sendMsg()
   {
     this.like.emit();
+  }
+  updateChild()
+  {
+    this.htmlcontent=this.htmlcontent;
+  }
+  mySwitch='case1';
+  userType=['admin','user','student'];
+  selectedType='';
+  changeCase()
+  {
+    this.mySwitch='';
   }
 }
--- Typescript/Angular/first_app/src/app/super/super.component.ts
+++ Typescript/Angular/first_app/src/app/super/super.component.ts
@@ -1,35 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { DerviedComponent } from './dervied/dervied.component';
 
 @Component({
   selector: 'app-super',
   templateUrl: './super.component.html',
   styleUrl: './super.component.css'
 })
-export class SuperComponent {
-  protected objs:{id:number,name:string,age:number}[]=[
+export class SuperComponent implements AfterViewInit {
+  
+    ngAfterViewInit():void
     {
-      id:1,
-      name:"Ahmed",
-      age:21
+      this.updateChild("<h1>helloooooooo from parent</h1>");
+    }
+    updateChild(content:string)
+    {
+      if(this.mychild)
+      {
+        this.mychild.htmlcontent=content;
+        this.cdr.detectChanges();
+      }
+    }
+    constructor(private cdr:ChangeDetectorRef) { }
+  
+  protected objs: { id: number, name: string, age: number }[] = [
+    {
+      id: 1,
+      name: "Ahmed",
+      age: 21
     },
     {
-      id:2,
-      name:"Sally",
-      age:21
+      id: 2,
+      name: "Sally",
+      age: 21
     },
     {
-      id:3,
-      name:"Soha",
-      age:21
+      id: 3,
+      name: "Soha",
+      age: 21
     }
   ];
-  protected msg:string="";
-  onLike()
-  {
-    this.msg="hello";
+  protected msg: string = "";
+  onLike() {
+    this.msg = "hello";
   }
-  addProduct()
-  {
-    this.objs.push({id:4,name:"Sahil",age:21});
+  addProduct() {
+    this.objs.push({ id: 4, name: "Sahil", age: 21 });
   }
+  chagneParentURL() {
+    this.updateChild("<h1>helloooooooo from parent changed</h1>");
+  }
+  @ViewChild(DerviedComponent)
+  mychild!: DerviedComponent;
+
 }
